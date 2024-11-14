c3854125c9b96495a9a5c8a5c8d8af9f0bc2fda0
IvanCuervoMarcos <uo278095@uniovi.es>
2024-11-14 22:24:58 +0100

    Cambiado tipo de dato Fecha y borrado atributo Hora

--- src/CentralBackend/DTOs/PlanVueloDTO.cs
+++ src/CentralBackend/DTOs/PlanVueloDTO.cs
@@ -2,8 +2,8 @@
 {
     public class PlanVueloDTO
     {
-        public DateTime FechaInicio { get; set; }
-        public DateTime FechaFin { get; set; }
+        public string FechaInicio { get; set; }
+        public string FechaFin { get; set; }
         public int ControlManual { get; set; }
         public int DronId { get; set; }
         public int RutaId { get; set; }
