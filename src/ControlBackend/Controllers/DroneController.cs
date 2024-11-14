8113d44ba8a96e81c051127a320ec18ae9ee0fc9
David González Vázquez <UO277437@uniovi.es>
2024-11-14 20:21:11 +0100

    First version of DroneController in ControlBackend

---
+++ src/ControlBackend/Controllers/DroneController.cs
@@ -0,0 +1,48 @@
+using Serilog;
+using DroneController;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ControlBackend.Controllers
+{
+    [ApiController]
+    [Route("ControlBackend/[controller]")]
+    public class DroneController : ControllerBase
+    {
+        readonly IBroker _pubsub_dron;
+
+        public DroneController(IBroker pubsub)
+        {
+            _pubsub_dron = pubsub;
+        }
+
+
+        [HttpGet("status/{id}")]
+        public ActionResult<IEnumerable<string>> GetDroneStatus([FromRoute] int id)
+        {
+            // Validar el ID
+            if (id <= 0)
+            {
+                Log.Warning("Invalid drone ID {0} received", id);
+                return BadRequest("Invalid drone ID");
+            }
+
+            try {
+            // Publicar el comando STATUS_CMD
+            Log.Information("Status request for drone {0} received from CENTRALBACKEND", id);
+
+            //Parseamos los datos
+            DroneCommand dron = new DroneCommand()
+            {
+
+                Command = DroneCommand.STATUS_CMD,
+                Arguments = ""
+            };
+
+            //Publicamos en la cola la peticion de datos del dron
+            _pubsub_dron.Publish("ControlBackend.Controller." + id, dron.Encode());
+
+            // Retorno de la respuesta
+            return Ok("");
+        }
+    }
+}
