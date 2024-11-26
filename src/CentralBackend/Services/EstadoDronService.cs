c8e84bb27d479abf19f0b674df2d01df1b2120ce
antosalviso <uo277724@uniovi.es>
2024-11-26 18:54:49 +0100

    merge con main

--- src/CentralBackend/Services/EstadoDronService.cs
+++ src/CentralBackend/Services/EstadoDronService.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CentralBackend.Hubs;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using System.Text.Json;
 
 namespace CentralBackend.Services
 {
@@ -6,6 +9,7 @@ namespace CentralBackend.Services
     public class EstadoDronService
     {
         readonly HttpClient _client;
+        private readonly IHubContext<UpdateMapHub> _hubContext;
 
 
         // Define the constructor that takes both a database context and an HTTP client as arguments
@@ -27,5 +31,26 @@ namespace CentralBackend.Services
             
         }
 
+        public async Task ActualizarPosicionDron(DroneInfo dronEstado)
+        {
+            try
+            {
+                // Serializar la información del dron y enviarla al controlador UpdateMapController
+                var content = new StringContent(JsonSerializer.Serialize(dronEstado), System.Text.Encoding.UTF8, "application/json");
+                var response = await _client.PostAsync("api/updatemap/actualizar", content);
+
+                if (!response.IsSuccessStatusCode)
+                {
+                    throw new Exception($"Error al actualizar la posición del dron: {response.StatusCode}");
+                }
+
+                Console.WriteLine($"Dron actualizado correctamente");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error en ActualizarPosicionDron: {ex.Message}");
+                throw;
+            }
+        }
     }
 }
